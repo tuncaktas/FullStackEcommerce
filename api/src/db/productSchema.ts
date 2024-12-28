108f11636172e0312402150fda26849e5b8f79aa
tuncaktas <111659476+tuncaktas@users.noreply.github.com>
2024-12-28 18:28:59 +0300

    Product CRUD

--- api/src/db/productSchema.ts
+++ api/src/db/productSchema.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, varchar, text, doublePrecision } from "drizzle-orm/pg-core";
 
-export const productTable = pgTable("products", {
+export const productsTable = pgTable("products", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
     description: text(),
