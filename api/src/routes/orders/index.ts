98025fd2c984d6ae35bea573a6cd3c179e6e589d
tuncaktas <111659476+tuncaktas@users.noreply.github.com>
2025-01-17 01:29:22 +0300

    Orders and order items create

--- api/src/routes/orders/index.ts
+++ api/src/routes/orders/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { createOrder } from './ordersController.js'
 import { validateData } from "../../middlewares/validationMiddleware.js";
-import { insertOrderSchema } from "../../db/ordersSchema.js";
+import { insertOrderWithItemsSchema } from "../../db/ordersSchema.js";
 import { verifyToken } from "../../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.post('/', verifyToken, validateData(insertOrderSchema), createOrder);
+router.post('/', verifyToken, validateData(insertOrderWithItemsSchema), createOrder);
 
 export default router;
