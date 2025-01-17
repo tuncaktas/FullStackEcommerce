7debcdba8e39e0e380e19ec03767acbf9eb729b4
tuncaktas <111659476+tuncaktas@users.noreply.github.com>
2025-01-17 05:40:20 +0300

    Products list structure

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,10 +1,15 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import products from '../assets/products.json';
+import ProductListItem from '../components/ProductListItem';
+
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
-  )
+      <FlatList 
+        data={products} 
+        renderItem={({ item }) => 
+          <ProductListItem product={item} />
+        }
+      />
+  );
 }
